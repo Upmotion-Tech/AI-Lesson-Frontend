4b4fd998a761fe5cc86d43fe3137569fab0f98f1
AliNaeem1228 <alinaeemgohan1228@yahoo.com>
2025-11-19 19:55:17 +0500

    code

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+console.log(API_BASE_URL);
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
