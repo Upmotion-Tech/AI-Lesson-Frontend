9017836e3e1208f1e0db788a84f4f2a120968e36
Upmotion-Tech <unas@theupmotion.com>
2026-03-19 20:06:02 +0500

    codoe

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: "force-close",
+      closeBundle() {
+        process.exit(0);
+      },
+    },
+  ],
 });
